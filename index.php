bd5eb0445551cd253d1c92cc83633fdbacf64861
marcodeiulio <mdeiulio@icloud.com>
2022-02-24 17:31:32 +0100

    - Added possibility to censor part of the text

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 $my_badword = $_GET['badword'];
 
+$par = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio qui modi repellendus asperiores optio sequi aperiam aliquam eius, enim suscipit? Obcaecati, necessitatibus quae voluptatem eum consectetur corporis sunt unde soluta!</p>';
+
+$censored_par = str_replace($my_badword, '***', $par);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,13 +17,14 @@ $my_badword = $_GET['badword'];
 	<title>Badwords</title>
 </head>
 <body>
-	<h1>Testo da censurare</h1>
-	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio qui modi repellendus asperiores optio sequi aperiam aliquam eius, enim suscipit? Obcaecati, necessitatibus quae voluptatem eum consectetur corporis sunt unde soluta!</p>
-	<h3>Quale parola vuoi censurare?</h3>
+	<h1>Testo da revisionare</h1>
+	<p><?php echo $par ?>
 	<form action="index.php">
 		<input type="text" placeholder="Inserisci la parola da censurare" name="badword">
 		<button role="submit">Invia</button>
 	</form>
 <h3>Hai scelto di censurare la parola "<?php echo $my_badword ?>"</h3>
+<h3>Il paragrafo revisionato è il seguente</h3>
+<p><?php echo $censored_par ?></p>
 </body>
 </html>
